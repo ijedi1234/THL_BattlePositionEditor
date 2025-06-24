80e2e0716c8a7bc2c53a05c06a249f5283e302c3
ijedi1234 <thegreatijedi@gmail.com>
2025-06-23 19:03:11 -0500

    remove pics logic

--- thl_battlepositioneditor/src/components/MapGrid.tsx
+++ thl_battlepositioneditor/src/components/MapGrid.tsx
@@ -1,22 +1,6 @@
 import { useState } from 'react'
 import { Button, ButtonGroup } from 'reactstrap'
 import './MapGridCell.css'
-import chr01 from '../../public/images/chr01.png'
-import chr02 from '../../public/images/chr02.png'
-import chr03 from '../../public/images/chr03.png'
-import chr04 from '../../public/images/chr04.png'
-import chr05 from '../../public/images/chr05.png'
-import chr06 from '../../public/images/chr06.png'
-import chr07 from '../../public/images/chr07.png'
-import chr08 from '../../public/images/chr08.png'
-import chr09 from '../../public/images/chr09.png'
-import chr10 from '../../public/images/chr10.png'
-import chr11 from '../../public/images/chr11.png'
-import chr12 from '../../public/images/chr12.png'
-import chr13 from '../../public/images/chr13.png'
-import chr14 from '../../public/images/chr14.png'
-import chr15 from '../../public/images/chr15.png'
-import chr16 from '../../public/images/chr16.png'
 
 interface MapGridParams {
     groundRecords: object,
@@ -31,24 +15,6 @@ function MapGrid(props: MapGridParams) {
     const layer = props.layer;
     const waveNumber = props.waveNumber;
 
-    const byte2Image = new Map();
-    byte2Image.set(101, chr01)
-    byte2Image.set(102, chr02)
-    byte2Image.set(103, chr03)
-    byte2Image.set(104, chr04)
-    byte2Image.set(105, chr05)
-    byte2Image.set(106, chr06)
-    byte2Image.set(107, chr07)
-    byte2Image.set(108, chr08)
-    byte2Image.set(109, chr09)
-    byte2Image.set(110, chr10)
-    byte2Image.set(111, chr11)
-    byte2Image.set(112, chr12)
-    byte2Image.set(113, chr13)
-    byte2Image.set(114, chr14)
-    byte2Image.set(115, chr15)
-    byte2Image.set(116, chr16)
-
     const byte2Color = new Map();
     byte2Color.set(0, 'cellEnemyOnly');
     byte2Color.set(1, 'cellWalkable');
@@ -58,10 +24,6 @@ function MapGrid(props: MapGridParams) {
     byte2Color.set(99, 'cellCameraCenterNotPassable');
 
     const ConvertByteGround = (byte: number) => {
-        const image = byte2Image.get(byte)
-        if (image != undefined) {
-            return <img className="cellImage" src={image} />
-        }
         const colorClass = byte2Color.get(byte);
         if (colorClass != undefined) {
             return <div className={colorClass}></div>
